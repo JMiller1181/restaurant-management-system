b27467ec8135b062a8f2dbee8ff90c3a29f23170
Javier <d2j5@hotmail.com>
2023-06-06 15:34:05 -0400

    Table code

--- src/test/java/org/restaurant/models/TableTest.java
+++ src/test/java/org/restaurant/models/TableTest.java
@@ -4,6 +4,8 @@ package org.restaurant.models;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.restaurant.models.Table.assignTableByPartySize;
+import static org.restaurant.models.Table.reserveTableByPartySize;
 
 class TableTest {
 
@@ -37,5 +39,45 @@ class TableTest {
         // The status should still be OCCUPIED and cannot be assigned again
         assertEquals(Table.TableStatus.OCCUPIED, table.getStatus());
     }
+
+    @Test
+    public void testAssignTableByPartySize() {
+        // Create an array of tables
+        Table[] tables = new Table[3];
+        tables[0] = new Table(1, 2);
+        tables[1] = new Table(2, 4);
+        tables[2] = new Table(3, 6);
+
+        // Assign a table with party size 3
+        assignTableByPartySize(tables, 3);
+
+        // Check if a table has been assigned
+        boolean tableAssigned = false;
+        for (Table table : tables) {
+            if (table.getStatus() == Table.TableStatus.OCCUPIED) {
+                tableAssigned = true;
+                break;
+            }
+        }
+
+        assertTrue(tableAssigned, "A table should have been assigned.");
+    }
+
+    @Test
+    public void testReserveTableByPartySize() {
+        // Create an array of tables
+        Table[] tables = new Table[3];
+        tables[0] = new Table(1, 2);
+        tables[1] = new Table(2, 4);
+        tables[2] = new Table(3, 6);
+
+        // Reserve a table with party size 3
+        Table reservedTable = reserveTableByPartySize(tables, 3);
+
+        // Check if a table has been reserved
+        assertNotNull(reservedTable, "A table should have been reserved.");
+        assertEquals(Table.TableStatus.RESERVED, reservedTable.getStatus(), "The reserved table should have status 'RESERVED'.");
+    }
+
 }
 
